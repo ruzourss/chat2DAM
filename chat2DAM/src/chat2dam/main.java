0243db1e026273cef1d74fe89d490396fdc0832d
Tote <Tote@Tote-PC>
2015-12-16 12:15:22 +0100

    crear el proyecto y añador la clase main

---
+++ chat2DAM/src/chat2dam/main.java
@@ -0,0 +1,16 @@
+package chat2dam;
+
+/**
+ * Clase main principal
+ * @author Tote
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
